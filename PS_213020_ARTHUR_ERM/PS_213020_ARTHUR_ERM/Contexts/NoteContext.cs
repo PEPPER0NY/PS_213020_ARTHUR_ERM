daf8b29d7663936672e376fc175328ca62ee7952
rusumuhu <83660075+rusumuhu@users.noreply.github.com>
2021-10-02 17:56:20 +0300

    Что-то сделали

---
+++ PS_213020_ARTHUR_ERM/PS_213020_ARTHUR_ERM/Contexts/NoteContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PS_213020_ARTHUR_ERM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS_213020_ARTHUR_ERM.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ PS_213020_ARTHUR_ERM/PS_213020_ARTHUR_ERM/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS_213020_ARTHUR_ERM.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CretaeDate { get; set; }
+        public DateTime? ChangeDate { get; set; }
+
+    }
+}
